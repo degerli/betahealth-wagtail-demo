20a4fccd866a214d02032f811d59e712d75fc0e5
Kerin Cosford <cosford@gmail.com>
2016-09-27 11:59:34 +0100

    Enabled WhiteNoise

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -5,6 +5,7 @@ from .base import *
 DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 try:
     from .local import *

b7777ae2f07e44df4e065a2378eb9746faae2baa
Kerin Cosford <cosford@gmail.com>
2016-09-27 11:56:22 +0100

    added .herokuapp.com to ALLOWED_HOSTS

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -3,8 +3,8 @@ import dj_database_url
 from .base import *
 
 DEBUG = False
-
 DATABASES['default'] = dj_database_url.config()
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 try:
     from .local import *

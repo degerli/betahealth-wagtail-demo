d1b7d81c6835d54d1bd14eedb32a4036210b6bdc
Marco Fucci <marcofucci@gmail.com>
2016-09-27 14:55:11 +0100

    Fix S3 settings

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -22,11 +22,10 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'demo.storages.StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'demo.storages.MediaStorage'
 
 try:
     from .local import *

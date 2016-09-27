ad5eaeb1903772bbc8198099b10cc3c71af42dca
Kerin Cosford <cosford@gmail.com>
2016-09-27 11:32:21 +0100

    removed apparently invalid kwarg for dj_database_url.config

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = False
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 try:
     from .local import *

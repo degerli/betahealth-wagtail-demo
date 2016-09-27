f3cc7cb88dd2c42d82abf2697faa195d416a122a
Kerin Cosford <cosford@gmail.com>
2016-09-27 14:14:19 +0100

    Configured django to use S3 for static assets and user-uploaded files

--- demo/settings/heroku.py
+++ demo/settings/heroku.py
@@ -1,11 +1,32 @@
 from __future__ import absolute_import, unicode_literals
+import os
 import dj_database_url
 from .base import *
 
+
 DEBUG = False
+
 DATABASES['default'] = dj_database_url.config()
+
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'demo.storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'demo.storages.MediaStorage'
 
 try:
     from .local import *

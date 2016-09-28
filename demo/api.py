19dd20d422b92fc7e6023f82a65939d82ecf4f65
Marco Fucci <marcofucci@gmail.com>
2016-09-28 11:57:45 +0100

    Restore get by id and add get by url_path
    So that we don't mess around with the default wagtail logic

--- demo/api.py
+++ demo/api.py
@@ -1,7 +1,3 @@
-from django.conf.urls import url
-
-from rest_framework.generics import get_object_or_404
-
 from wagtail.api.v2.endpoints import PagesAPIEndpoint as WagtailPagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.wagtailimages.api.v2.endpoints import ImagesAPIEndpoint
@@ -11,56 +7,13 @@ api_router = WagtailAPIRouter('wagtailapi')
 
 
 class PagesAPIEndpoint(WagtailPagesAPIEndpoint):
-    lookup_field = 'url_path'
-    lookup_url_kwarg = 'pk'
+    meta_fields = WagtailPagesAPIEndpoint.meta_fields + [
+        'url_path'
+    ]
 
-    def _get_resolved_url_path(self):
-        """
-        The url_path passed via URL does not include the home slug so we are
-        constructing it here before returning it.
-        """
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        relative_url_path = self.kwargs[lookup_url_kwarg]
-        return '/{}/{}/'.format(self.request.site.root_page.slug, relative_url_path)
-
-    def get_object(self):
-        """
-        Returns the object the view is displaying.
-
-        You may want to override this if you need to provide non-standard
-        queryset lookups.  Eg if objects are referenced using multiple
-        keyword arguments in the url conf.
-        """
-        queryset = self.filter_queryset(self.get_queryset())
-
-        # Perform the lookup filtering.
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        assert lookup_url_kwarg in self.kwargs, (
-            'Expected view %s to be called with a URL keyword argument '
-            'named "%s". Fix your URL conf, or set the `.lookup_field` '
-            'attribute on the view correctly.' %
-            (self.__class__.__name__, lookup_url_kwarg)
-        )
-
-        filter_kwargs = {self.lookup_field: self._get_resolved_url_path()}
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        return obj
-
-    @classmethod
-    def get_urlpatterns(cls):
-        """
-        This returns a list of URL patterns for the endpoint
-        """
-        return [
-            url(r'^$', cls.as_view({'get': 'listing_view'}), name='listing'),
-            url(r'^(?P<pk>[\w/-]+)/$', cls.as_view({'get': 'detail_view'}), name='detail'),
-        ]
+    listing_default_fields = WagtailPagesAPIEndpoint.listing_default_fields + [
+        'url_path'
+    ]
 
 
 api_router.register_endpoint('pages', PagesAPIEndpoint)
